8df4c91beb8eeeedfd83b9587815cee748d8f912
Pechenyice <german.iskhakov@mail.ru>
2020-11-20 01:28:11 +0300

    new chat fix

--- js/chat.js
+++ js/chat.js
@@ -9,14 +9,14 @@ window.addEventListener('DOMContentLoaded', () => {
     let outMessage = '<div class="chatMessage outMessage"><div class="messageContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit  adipiscing elit </div><div class="messageTime">23:04</div></div>';
 
     window.addEventListener('scroll', () => {
-        console.log(window.innerHeight/2)
+        // console.log(window.innerHeight/2)
         if (animContainer.getBoundingClientRect().top < window.innerHeight) {
             if (!ready) {
-                console.log("asd")
+                // console.log("asd")
                 setTimeout(addOutMessage, 4);
-                setTimeout(addInMessage, 1500);
-                setTimeout(addOutMessage, 3000);
-                setTimeout(addInMessage, 4500);
+                setTimeout(addInMessage, 2000);
+                setTimeout(addOutMessage, 4000);
+                setTimeout(addInMessage, 6000);
                 ready = 1;
             }
         }
@@ -48,5 +48,5 @@ function addOutMessage() {
     setTimeout(() => {
         animContainer.getElementsByClassName('chatMessage')[animContainer.getElementsByClassName('chatMessage').length - 1].style.opacity = '1';
         animContainer.getElementsByClassName('chatMessage')[animContainer.getElementsByClassName('chatMessage').length - 1].style.maxHeight = '100px';
-    }, 4);
+    }, 25);
 }
